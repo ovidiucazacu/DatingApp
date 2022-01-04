ada388b0a035e289ae86b32e00e5f007bbe4c66a
ovidiu <ovidiu@dianoia-it.com>
2022-01-04 09:44:45 +0200

    End of section 10

--- client/src/app/_services/busy.service.ts
+++ client/src/app/_services/busy.service.ts
@@ -15,6 +15,14 @@ export class BusyService {
       type: 'line-scale-party',
       bdColor: 'rgba(255,255,255,0)',
       color: '#333333'
-    })
+    });
+  }
+
+  idle(){
+    this.busyRequestCount--;
+    if(this.busyRequestCount <= 0){
+      this.busyRequestCount = 0;
+      this.spinnerService.hide();
+    }
   }
 }
