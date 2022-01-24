b9feebf0d7ebadc054775254083de7fa3f831275
ovidiu <ovidiu@dianoia-it.com>
2022-01-24 14:58:06 +0200

    End of section 14

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -29,14 +29,17 @@ export class ErrorInterceptor implements HttpInterceptor {
                   }
                 }
                 throw modalStateErrors.flat();
+              }else if(typeof(error.error) === 'object'){
+                this.toastr.error(error.statusText, error.status);
+                //this.toastr.error(error.error === null ? "Bad request" : error.error, error.status)
               }else{
-                this.toastr.error(error.error === null ? "Bad request" : error.error, error.status)
+                this.toastr.error(error.error, error.status);
               }
               break;
             
             case 401:
-              //this.toastr.error(error.statusText, error.status);
-              this.toastr.error(error.error === null ? "Unauthorized" : error.error, error.status)
+              this.toastr.error(error.statusText, error.status);
+              //this.toastr.error(error.error === null ? "Unauthorized" : error.error, error.status)
               break;
 
             case 404:
